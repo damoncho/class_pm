e11d32e2862d61ba38bf1405629dda6a5d8058df
damoncho <99659620+damoncho@users.noreply.github.com>
2024-11-20 23:17:10 +0900

    fix

--- src/data/curriculum_obsidian.ts
+++ src/data/curriculum_obsidian.ts
@@ -7,13 +7,13 @@ export const curriculumData = [
         id: "course-1-1",
         title: "기본 사용 가이드",
         thumbnailUrl: "https://i.vimeocdn.com/video/1951107377-a9db48c5ddcbb7bd204a0021326e3faacd418bd99ebaadb05115dca1788d0052-d_300x169?r=pad",
-        landingUrl: "https://damon.im/planner-developer-designer-it-roles-summary/"
+        landingUrl: "https://damon.im/obsidian-class-1-1/"
       },
       {
         id: "course-1-2",
         title: "디자인 테마 세팅",
         thumbnailUrl: "https://i.vimeocdn.com/video/1951857658-cdc5600b9080684b46860ae1eaa2e58a05032e6487a2b8a25b82be02ce85d001-d_300x169?r=pad",
-        landingUrl: "https://damon.im/it-product-manager-roles/"
+        landingUrl: "https://damon.im/obsidian-class-1-2/"
       }
     ]
   },
