6b6a2c8b7fe2a440cd08b8c0b69327dded7991dc
damoncho <99659620+damoncho@users.noreply.github.com>
2024-11-20 22:55:32 +0900

    fix 3

--- src/app/obsidian/page.tsx
+++ src/app/obsidian/page.tsx
@@ -26,7 +26,7 @@ export default function StructuredCurriculum() {
       <section className="py-24">
           <div className="container mx-auto text-center px-4">
             <h1 className="text-4xl font-bold mb-6">Obsidian 온라인 클래스</h1>
-            <p className="text-xl max-w-2xl mx-auto">온라인 클래스는 <span className="bg-[#D3FFA3] text-black p-1 rounded">오프라인 수강생 대상으로 무료 공개</span>합니다.</p>
+            <p className="text-xl max-w-2xl mx-auto">온라인 클래스는 <span className="bg-yellow-200 text-black p-1 rounded">오프라인 수강생 대상으로 무료 공개</span>합니다.</p>
           </div>
         </section>
 
