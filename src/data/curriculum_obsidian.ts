6e10a52652975f619a7193c0da8536e0e3d592d9
damoncho <99659620+damoncho@users.noreply.github.com>
2024-11-25 01:01:28 +0900

    img link

--- src/data/curriculum_obsidian.ts
+++ src/data/curriculum_obsidian.ts
@@ -24,7 +24,7 @@ export const curriculumData = [
       {
         id: "course-2-1",
         title: "모바일 퀵 캡쳐 Telegram Sync",
-        thumbnailUrl: "https://i.vimeocdn.com/video/1952803589-c1612cd113a78176da9ebc4b01c6c374c06f0e48c323a70b036935a850d4be61-d_300x169?r=pad",
+        thumbnailUrl: "https://i.vimeocdn.com/video/1953822290-751e814e0108aa357614231483c73c37e7eab715cb5b5535259e6ffecfd6d93b-d_300x169?r=pad",
         landingUrl: "https://damon.im/obsidian-class-2-1/"
       },
     ]
