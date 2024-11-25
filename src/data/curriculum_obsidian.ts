ff28ec08eed393f9f9044ba746a5122fdb4da7a7
damoncho <99659620+damoncho@users.noreply.github.com>
2024-11-25 22:39:47 +0900

    add

--- src/data/curriculum_obsidian.ts
+++ src/data/curriculum_obsidian.ts
@@ -23,10 +23,16 @@ export const curriculumData = [
     courses: [
       {
         id: "course-2-1",
-        title: "모바일 퀵 캡쳐 Telegram Sync",
+        title: "Telegram Sync - 텔레그램 앱을 통한 실시간 메모",
         thumbnailUrl: "https://i.vimeocdn.com/video/1953923446-f86c46ba75f2bed180fdc2a095d8518100c66689b352e8aa367ad4b63ef55ba8-d_300x169?r=pad",
+        landingUrl: "https://damon.im/obsidian-class-2-1/"
+      },
+      {
+        id: "course-2-2",
+        title: "Journal Review - 일주일전, 1개월전, 1년전 노트 돌아보기",
+        thumbnailUrl: "https://i.vimeocdn.com/video/1954126599-eeae88fe5aab4267578bc09cbff6a709e7c53cb1ec56d218eb80f6d19ab9f24a-d_300x169?r=pad",
         landingUrl: "https://damon.im/obsidian-class-2-1/"
       },
     ]
   }
-];
+]
